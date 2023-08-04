ae139bf086550a9aff0ce82a81e657bdda3fb9fe
sgoldsmith <sgoldsmith@sgoldsmith-VirtualBox>
2023-08-04 18:25:08 -0400

    Fixed up 4 bit PCM player

--- asm/pcm4.as
+++ asm/pcm4.as
@@ -7,45 +7,41 @@
 ;
 
 global  _playPcm4Sid
-global  ncsv, cret, indir
-
 psect   text
 
 _playPcm4Sid:
-
-global  csv
-
-call    csv
-ld      l,(ix+6)        ; Sample start address
-ld      h,(ix+7)
-ld      e,(ix+8)        ; Sample length
-ld      d,(ix+9)
-rep1:                   ; Repeat
-ld      bc,0dd0dh       ; bc = CIA 2 ICR
-rep2:                   ; Repeat
-in      a,(c)           ; a = CIA 2 ICR value
-bit     0,a             ;
-jr      z,rep2          ; Until interrupt flag set
-ld      a,(hl)          ; a = sample byte
-rrca                    ; a = a div 16
-rrca                    ;
-rrca                    ;
-rrca                    ;
-and     0fh             ; 4 bit nibble
-ld      bc,0d418h       ; bc = SID volume address
-out     (c),a           ; Set volume
-ld      bc,0dd0dh       ; bc = CIA 2 ICR
-rep3:                   ; Repeat
-in      a,(c)           ; a = CIA 2 ICR value
-bit     0,a             ;
-jr      z,rep3          ; Until interrupt flag set
-ld      a,(hl)          ; a = sample byte
-and     0fh             ; a = a and 15
-ld      bc,0d418h       ; bc = SID volume address
-out     (c),a           ; Set volume
-inc     hl              ; hl = hl+1
-dec     e               ;
-jr      nz,rep1         ;
-dec     d               ; de = de-1
-jr      nz,rep1         ;until de = 0
-jp      cret
+        pop     bc              ; Return address
+        pop     hl              ; Sample start address
+        pop     de              ; Sample length
+        push    de
+        push    hl
+        push    bc
+        rep1:                   ; Repeat
+        ld      bc,0dd0dh       ; bc = CIA 2 ICR
+        rep2:                   ; Repeat
+        in      a,(c)           ; a = CIA 2 ICR value
+        bit     0,a             ;
+        jr      z,rep2          ; Until interrupt flag set
+        ld      a,(hl)          ; a = sample byte
+        rrca                    ; a = a div 16
+        rrca                    ;
+        rrca                    ;
+        rrca                    ;
+        and     0fh             ; 4 bit nibble
+        ld      bc,0d418h       ; bc = SID volume address
+        out     (c),a           ; Set volume
+        ld      bc,0dd0dh       ; bc = CIA 2 ICR
+        rep3:                   ; Repeat
+        in      a,(c)           ; a = CIA 2 ICR value
+        bit     0,a             ;
+        jr      z,rep3          ; Until interrupt flag set
+        ld      a,(hl)          ; a = sample byte
+        and     0fh             ; a = a and 15
+        ld      bc,0d418h       ; bc = SID volume address
+        out     (c),a           ; Set volume
+        inc     hl              ; hl = hl+1
+        dec     e               ;
+        jr      nz,rep1         ;
+        dec     d               ; de = de-1
+        jr      nz,rep1         ; Until de = 0
+        ret
--- src/demo/playpcm4.c
+++ src/demo/playpcm4.c
@@ -16,19 +16,6 @@
 #include <sys.h>
 
 /*
- Load file into buffer.
- */
-void load(char *fileName, ushort fileSize, uchar *buffer) {
-	FILE *rawFile;
-	if ((rawFile = fopen(fileName, "rb")) != NULL) {
-		printf("\nReading %s, %d bytes", fileName, fileSize);
-		fread(buffer, sizeof(uchar), fileSize, rawFile);
-		fclose(rawFile);
-	} else
-		puts("\nUnable to open file.");
-}
-
-/*
  Display program help.
  */
 void dispHelp() {
@@ -38,9 +25,45 @@ void dispHelp() {
 }
 
 /*
+ Set CIA 2 TOD clock.
+ */
+void setCiaTod(uchar hour, uchar min, uchar sec, uchar tenth) {
+	/* Bit 7 = 0 sets TOD clock */
+	outp(cia2CtrlRegB, inp(cia2CtrlRegB) & 0x7f);
+	/* CIA 2 TOD hour */
+	outp(cia2TodHrs, hour);
+	/* CIA 2 TOD minute */
+	outp(cia2TodMin, min);
+	/* CIA 2 TOD second */
+	outp(cia2TodSec, sec);
+	/* CIA 2 TOD tenth second */
+	outp(cia2TodTen, tenth);
+}
+
+/*
+ * Convert bcd byte to 2 char base 10.
+ */
+void todToChar(uchar bcd, char *str) {
+	str[0] = (bcd >> 4) + 48;
+	str[1] = (bcd & 0x0f) + 48;
+}
+
+/*
+ Display current time in SS:TT format using CIA 2's TOD clock.
+ */
+void dispTime() {
+	char todStr[6];
+	todToChar(inp(cia2TodSec), &todStr[0]);
+	todStr[2] = ':';
+	todToChar(inp(cia2TodTen), &todStr[3]);
+	todStr[5] = '\0';
+	printf("%s\n", todStr);
+}
+
+/*
  Return file size or 0 for error.
  */
-ushort getFileSize(char *fileName) {
+ulong getFileSize(char *fileName) {
 	struct stat statRec;
 	if (stat(fileName, &statRec) == 0) {
 		return statRec.st_size;
@@ -53,17 +76,20 @@ ushort getFileSize(char *fileName) {
  Swap nibbles in buffer for inverted RAWs.
  */
 void swapNibbles(uchar *buffer, ushort len) {
-	register ushort i;
+	ushort i;
+	printf("Swapping nibbles, ");
+	setCiaTod(0, 0, 0, 0);
 	for (i = 0; i < len; i++)
 		buffer[i] = (buffer[i] << 4) | (buffer[i] >> 4);
+	dispTime();
 }
 
 /*
  Clear ICR and start timer A in continuous mode using Hz value.
  */
 void startTimer(ushort hz) {
+	/* ciaMs is ~1 KHz */
 	ushort timerA = ciaMs / (hz / 1000);
-	initCia();
 	/* CIA 2 Timer A lo */
 	outp(cia2TimerALo, (uchar) timerA);
 	/* CIA 2 Timer A hi */
@@ -78,14 +104,35 @@ void startTimer(ushort hz) {
 void play(uchar *buffer, ushort len, ushort hz) {
 	/* Start HZ timer */
 	startTimer(hz);
+	printf("Playing, ");
+	setCiaTod(0, 0, 0, 0);
 	/* Play sample */
 	playPcm4Sid(buffer, len);
+	dispTime();
 }
 
+/*
+ Load file into buffer.
+ */
+void load(char *fileName, ulong fileSize, uchar *buffer) {
+	FILE *rawFile;
+	if ((rawFile = fopen(fileName, "rb")) != NULL) {
+		printf("\nReading %s, %d bytes, ", fileName, fileSize);
+		setCiaTod(0, 0, 0, 0);
+		fread(buffer, sizeof(uchar), fileSize, rawFile);
+		fclose(rawFile);
+		dispTime();
+	} else
+		puts("\nUnable to open file.");
+}
+
+/*
+ * Main function.
+ */
 main(int argc, char *argv[]) {
 	uchar *buffer;
 	ushort minHz = 3999, maxHz = 15001, hz, maxFileSize = 32768;
-	ushort fileSize;
+	ulong fileSize;
 	/* Make sure we have 3 or more params */
 	if (argc > 2) {
 		/* Convert hz param to unsigned short */
@@ -101,11 +148,11 @@ main(int argc, char *argv[]) {
 				/* Allocate buffer */
 				buffer = (uchar*) malloc(fileSize);
 				if (buffer != NULL) {
+					load(argv[1], fileSize, buffer);
 					if (strcmp(argv[3], "sn") == 0) {
-						printf("Swapping nibbles, ");
 						swapNibbles(buffer, fileSize);
 					}
-					printf("Playing");
+					initCia();
 					play(buffer, fileSize, hz);
 					free(buffer);
 					doneCia();
