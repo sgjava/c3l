3965d9280e233dd511e4084630dbf0d1e80ed0bb
sgoldsmith <sgoldsmith@sgoldsmith-VirtualBox>
2024-05-14 21:02:33 -0400

    Use irq1 extern instead of code lookup

--- src/app/vicsplit.c
+++ src/app/vicsplit.c
@@ -16,21 +16,7 @@
 #include <string.h>
 #include <vic.h>
 
-/*
- * Use this to find the Z80 signature for custom interrupt code. len is returned
- * if byte sequence not found.
- */
-unsigned int findBytes(unsigned char bytes[], unsigned char *mem, unsigned int len, unsigned char size) {
-	unsigned int i;
-	for (i = 0; i <= len - size; i++) {
-		if (memcmp(&mem[i], bytes, size) == 0) {
-			/* Match found starting at index i */
-			return i;
-		}
-	}
-	/* No match found */
-	return len;
-}
+extern void irq1(void);
 
 /*
  * Initialize key scan, screen and bitmap.
@@ -61,20 +47,12 @@ void done() {
  * Find IRQ code signature and run demo. If assembler code changes then signature must change as well.
  */
 void run(unsigned char *memEnd, console *con, bitmap *bmp) {
-	extern void *irq1;
 	unsigned int found;
 	unsigned char *memStart = (unsigned char*) 0x0100;
 	/* This is the z80 signature used to find the custom interrupt code inside vicSplitScr */
 	static unsigned char target[] = { 0xf5, 0xc5, 0xe5, 0x01, 0x12, 0xd0 };
-	/* Find custom interrupt code in program */
-	found = findBytes(target, memStart, memEnd - memStart, sizeof(target))
-			+ (unsigned int) memStart;
-	/* Make sure we find target code, but not target array */
-	if (found != (unsigned int) target) {
-		runGraphDemoI(con, bmp, found);
-	} else {
-		printf("\nIRQ code signature not found\n");
-	}
+	// Run demo using extern irq1
+	runGraphDemoI(con, bmp, irq1);
 }
 
 main() {
